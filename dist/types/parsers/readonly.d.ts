f1882848bf7f4f3c17be59b1530e533f85578e6c
Hernawan Faiz Abdillah <hernawan.faiz.abdillah@gmail.com>
2025-08-13 07:30:24 +0700

    chore: build

---
+++ dist/types/parsers/readonly.d.ts
@@ -0,0 +1,3 @@
+import { ZodReadonlyDef } from "zod/v3";
+import { Refs } from "../Refs.js";
+export declare const parseReadonlyDef: (def: ZodReadonlyDef<any>, refs: Refs) => import("../parseDef.js").JsonSchema7Type | undefined;
